fbdbf921acccc10a3a7e2e15aa861f620e7fda01
Maithe Saba <maithess04@gmail.com>
2025-05-05 17:10:48 -0300

    New constructor

--- src/projeto_sistema_produtos/models/person/Supplier.java
+++ src/projeto_sistema_produtos/models/person/Supplier.java
@@ -5,16 +5,20 @@ import projeto_sistema_produtos.models.address.Address;
 public class Supplier extends Person{
     private String description;
 
+    public Supplier(Role role, String name, String phone, String email, Address address) {
+    	super(role, name, phone, email, address);
+    }
+    
+    public Supplier(Role role, String name, String phone, String email, String description, Address address) {
+        super(role, name, phone, email, address);
+        this.description = description;
+    }
+    
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Supplier(Role role, String name, String phone, String email, String description, Address address) {
-        super(role, name, phone, email, address);
         this.description = description;
     }
 
