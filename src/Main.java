48f1d83905fe002eea064ae6e1d833fd205006d1
Maithe <126606136+MaitheSaba@users.noreply.github.com>
2025-05-14 21:24:41 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
 	
     public static void main(String[] args) throws Exception {
     	LoginView login = new LoginView(store);
-        login.show();
+        login.show(sc);
     }    
 }
