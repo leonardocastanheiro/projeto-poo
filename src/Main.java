432d44b39f77e2ad3e055a1aef6d98297b34d2d3
Maithe <126606136+MaitheSaba@users.noreply.github.com>
2025-05-14 21:17:15 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class Main {
 	private static Scanner sc;
+	private static Store store = new Store("Amazon");
 	
     public static void main(String[] args) throws Exception {
-    	LoginView login = new LoginView();
+    	LoginView login = new LoginView(store);
         login.show();
     }    
 }
